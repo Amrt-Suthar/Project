7d427509c408f841f76894ad84ea56ce5810cf98
Amrt-Suthar <amritsuthar935@gmail.com>
2026-04-06 18:22:31 +0530

    Initial project upload

---
+++ MERN-Expense-Tracker-main/frontend/src/pages/ExpenseTable.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const ExpenseTable = ({ expenses = [], deleteExpens }) => {
+    return (
+        <div className="expense-list">
+            {expenses.map((expense) => (
+                <div key={expense._id} className="expense-item">
+                    <button
+                        className="delete-button"
+                        onClick={() => deleteExpens(expense._id)}
+                    >
+                        X
+                    </button>
+
+                    <div className="expense-description">{expense.text}</div>
+
+                    <div
+                        className="expense-amount"
+                        style={{
+                            color: expense.amount > 0 ? '#27ae60' : '#c0392b'
+                        }}
+                    >
+                        ₹{expense.amount}
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default ExpenseTable;
